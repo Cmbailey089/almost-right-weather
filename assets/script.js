844d5fcc7417b71d1ec9886983a66a861df3beaf
Cmbailey089 <christopherbailey089@gmail.com>
2023-03-14 18:11:24 -0400

    tutor helped with api

--- assets/script.js
+++ assets/script.js
@@ -5,10 +5,9 @@ var lastCity = document.querySelector ("#preCity");
 
 var getWeather = function(city) {
     var APIkey = "195afc894ee5fa544eb826395ad34cb5"
-    var currentUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + APIkey;
+    // var currentUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + APIkey;
     var grabUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=' + APIkey;
-//    var url = 'api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=' +APIkey;
-//    var tracker = 'http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},1&limit=5&appid={API key}';
+
 // console.log(grabUrl)
     fetch(grabUrl)
    
@@ -22,24 +21,56 @@ var getWeather = function(city) {
          console.log(data);
 });
 
-var displayWeather = function (data){         
+var displayWeather = function (data){  
+  console.log(data.list[0])  
+  console.log(data.list[0].main.humidity)   
+  console.log(data.list[0].main.temp)  
+  console.log(data.list[0].wind.speed)
              var weatherRow = document.querySelector("#weatherTable");
-            weatherRow.innerHTML=data;
+            
+            for(var i=0;i<data.list.length; i++) {
+
+              if(i===0||i%8===0){
+                 var hum = document.createElement("div");
+             var temp = document.createElement("div");
+             var wind =document.createElement("div"); 
+             var sunIcon = "https://openweathermap.org/img/wn/"+ data.list[i].weather[0].icon +"@2x.png";
+             var imgEl = document.createElement("img");
+             imgEl.src=sunIcon
+
+
+             var forecast = document.createElement("div");
+             forecast.id=("forCard");
+
+
+            hum.innerHTML+="Humidity="+data.list[i].main.humidity;
+            temp.innerHTML+="tempertur="+data.list[i].main.temp;
+            wind.innerHTML+="wind="+data.list[i].wind.speed;
+            forecast.appendChild(hum);
+            forecast.appendChild(temp);
+            forecast.appendChild(wind);
+            forecast.appendChild(imgEl);
+            weatherRow.appendChild(forecast);
+
+
+
+              }
+            }
         }};
 
  function displayStorage () {
    
-    var city = localStorage.getItem("city");
+    var city = JSON.parse(localStorage.getItem("city"));
 
-    lastCity.textContent = city;
+    
        for(var i=0; i<city.length; i++) {
         var cities = city[i];
-
+console.log(cities);
         var li = document.createElement("li");
-        li.textContent =cities;
+        li.innerHTML =cities;
         li.setAttribute("data-index",i);
 
-        lastCity.appendChild("li");
+        lastCity.appendChild(li);
                  
 }
 };
@@ -48,10 +79,15 @@ fetchBtn.addEventListener('click',function(event) {
   event.preventDefault();
 
         var city = document.querySelector("#city").value;
-    
+   
 getWeather(city);
-      
-        localStorage.setItem("city",city);
+        
+        console.log(cityNames)
+        var cityNames = JSON.parse(localStorage.getItem("city"))||[];
+        cityNames.push(city);
+
+
+        localStorage.setItem("city",JSON.stringify(cityNames));
         displayStorage();
     });
 
